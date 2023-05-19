efd18928f112793ddf7f6348d51cf01280770777
Andrew Fry <andrew.fry@neddie.ploogie.co.uk>
2023-05-19 14:09:17 +0100

    Added cbrt()

--- evaluator/cbrt.go
+++ evaluator/cbrt.go
@@ -5,9 +5,9 @@ import (
 	"mwnci/object"
 	"mwnci/typing"
        )
-func mathSqrt(args ...object.Object) object.Object {
+func mathCbrt(args ...object.Object) object.Object {
 	if err := typing.Check(
-		"sqrt", args,
+		"cbrt", args,
 		typing.ExactArgs(1),
 	); err != nil {
 		return newError(err.Error())
@@ -15,20 +15,20 @@ func mathSqrt(args ...object.Object) object.Object {
 	switch arg := args[0].(type) {
 	case *object.Integer:
 		v := arg.Value
-		return &object.Float{Value: math.Sqrt(float64(v))}
+		return &object.Float{Value: math.Cbrt(float64(v))}
 	case *object.Float:
 		v := arg.Value
-		return &object.Float{Value: math.Sqrt(v)}
+		return &object.Float{Value: math.Cbrt(v)}
 	default:
-		return newError("argument to `sqrt` not supported, got=%s",
+		return newError("argument to `cbrt` not supported, got=%s",
 			args[0].Type())
 	}
 
 }
 
 func init() {
-	RegisterBuiltin("sqrt",
+	RegisterBuiltin("cbrt",
 		func(env *object.Environment, args ...object.Object) object.Object {
-			return (mathSqrt(args...))
+			return (mathCbrt(args...))
 		})
 }
