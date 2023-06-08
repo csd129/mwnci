93807d32ef41b5ecde1ac9ec0b781e1699a3fc71
Andrew Fry <andrew.fry@ploogie.co.uk>
2023-06-08 20:24:15 +0100

    A working slow ucount()

--- evaluator/ucount.go
+++ evaluator/ucount.go
@@ -3,10 +3,12 @@ package evaluator
 import (
 	"fmt"
 	"mwnci/object"
+	"mwnci/lexer"
+	"mwnci/parser"
 	"mwnci/typing"
 )
 
-func Ucount(args ...object.Object) object.Object {
+func Ucount(env *object.Environment, args ...object.Object) object.Object {
 	if err := typing.Check(
 		"ucount", args,
 		typing.ExactArgs(1),
@@ -14,18 +16,39 @@ func Ucount(args ...object.Object) object.Object {
 	); err != nil {
 		return newError(err.Error())
 	}
-	dict := make(map[string]int)
+	//	dict := make(map[string]int)
 	arr := args[0].(*object.Array)
+	dict := make(map[string]int)
+	longstring:="{"
 	for _,data := range arr.Elements{
 		HashData := fmt.Sprintf("%v", data)
 		dict[HashData] = dict[HashData] + 1
 	}
+	counter := 1
+	buildline:=""
+	dictlen:=len(dict)
+	for k, v := range dict {
+		if counter != dictlen {
+			buildline = fmt.Sprintf("\"%v\": %v,", k, v)
+		} else {
+			buildline = fmt.Sprintf("\"%v\": %v}", k, v)
+		}
+		longstring=fmt.Sprintf("%v%v", longstring, buildline)
+		counter++
+	}
+	l := lexer.New(string(longstring))
+	p := parser.New(l)
+	program := p.ParseProgram()
+	if len(p.Errors()) == 0 {
+		return (Eval(program, env))
+	}
+	
 	return FALSE
 }
 
 func init() {
 	RegisterBuiltin("yewcount",
 		func(env *object.Environment, args ...object.Object) object.Object {
-			return (Ucount(args...))
+			return (Ucount(env, args...))
 		})
 }
