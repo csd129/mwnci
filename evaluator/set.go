c44b9efef38129262475c76ca598f7cfa6006651
Andrew Fry <afry@neddie.ploogie.co.uk>
2023-06-26 21:12:28 +0100

    Merged set() & aset() one builtin for arrays and hashes

--- evaluator/set.go
+++ evaluator/set.go
@@ -10,24 +10,38 @@ func setFun(args ...object.Object) object.Object {
 	if err := typing.Check(
 		"set", args,
 		typing.ExactArgs(3),
-		typing.WithTypes(object.HASH_OBJ),
 	); err != nil {
 		return newError(err.Error())
 	}
-	key, ok := args[1].(object.Hashable)
-	if !ok {
-		return newError("key `set` into HASH must be Hashable, got=%s",
-			args[1].Type())
+	if args[0].Type() == object.HASH_OBJ {
+		key, ok := args[1].(object.Hashable)
+		if !ok {
+			return newError("key `set` into HASH must be Hashable, got=%s",
+				args[1].Type())
+		}
+		newHash := make(map[object.HashKey]object.HashPair)
+		hash := args[0].(*object.Hash)
+		for k, v := range hash.Pairs {
+			newHash[k] = v
+		}
+		newHashKey := key.HashKey()
+		newHashPair := object.HashPair{Key: args[1], Value: args[2]}
+		newHash[newHashKey] = newHashPair
+		return &object.Hash{Pairs: newHash}
 	}
-	newHash := make(map[object.HashKey]object.HashPair)
-	hash := args[0].(*object.Hash)
-	for k, v := range hash.Pairs {
-		newHash[k] = v
+	if args[0].Type() == object.ARRAY_OBJ {
+		arr := args[0].(*object.Array)
+		nums := arr.Copy()
+		elem := int(args[1].(*object.Integer).Value)
+		if (elem > len(arr.Elements)-1) || (elem < 0) {
+			return newError("IndexError: array index [%d] out of range ", elem)
+		} else {
+			val := args[2]
+			nums.Aset(elem, val)
+		}
+		return nums
 	}
-	newHashKey := key.HashKey()
-	newHashPair := object.HashPair{Key: args[1], Value: args[2]}
-	newHash[newHashKey] = newHashPair
-	return &object.Hash{Pairs: newHash}
+	return newError("argument to set() not supported, expected HASH or ARRAY, got=%s", args[0].Type())
 }
 
 func init() {
