db05b3bebe1564ee98f6f245dead6c2941e80597
Andrew Fry <andrew.fry@neddie.ploogie.co.uk>
2023-10-02 14:17:16 +0100

    Simple fucntion to clear screen (must find a better way)

---
+++ evaluator/cls.go
@@ -0,0 +1,29 @@
+package evaluator
+
+import (
+	"os"
+	"os/exec"
+	"mwnci/object"
+	"mwnci/typing"
+)
+
+func Cls(args ...object.Object) object.Object {
+	if err := typing.Check(
+		"cls", args,
+		typing.ExactArgs(0),
+	); err != nil {
+		return newError(err.Error())
+	}     
+	cmd := exec.Command("/bin/clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+
+	return NULL
+}
+
+func init() {
+	RegisterBuiltin("cls",
+		func(env *object.Environment, args ...object.Object) object.Object {
+			return (Cls(args...))
+		})
+}
