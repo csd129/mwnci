5608c014d285176be635fb9ff1d9cec0b94e555c
Andrew Fry <andrew.fry@neddie.ploogie.co.uk>
2023-11-06 13:19:16 +0000

    Added splithostport()

---
+++ evaluator/splithostport.go
@@ -0,0 +1,38 @@
+package evaluator
+
+import (
+	"net"
+	"mwnci/object"
+	"mwnci/typing"
+)
+
+// Change a mode of a file - note the second argument is a string
+// to emphasise octal.
+func SplitHP(args ...object.Object) object.Object {
+	if err := typing.Check(
+		"splithostport", args,
+		typing.ExactArgs(1),
+		typing.WithTypes(object.STRING_OBJ),
+	); err != nil {
+		return newError(err.Error())
+	}     
+
+
+	HostPort := args[0].Inspect()
+	host, port, err := net.SplitHostPort(HostPort)
+	if err != nil {
+		return newError(err.Error())
+	}
+	elements := make([]object.Object, 2)
+	elements[0] = &object.String{Value: host}
+	elements[1] = &object.String{Value: port}
+	return &object.Array{Elements: elements}
+
+}
+
+func init() {
+	RegisterBuiltin("splithostport",
+		func(env *object.Environment, args ...object.Object) object.Object {
+			return (SplitHP(args...))
+		})
+}
--- go.mod
+++ go.mod
@@ -8,4 +8,8 @@ require (
 	golang.org/x/term v0.12.0
 )
 
-require golang.org/x/sys v0.12.0 // indirect
+require (
+	golang.org/x/sys v0.12.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/mail.v2 v2.3.1 // indirect
+)
