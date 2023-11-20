4790d5ff07f13c4e3345f95d1be59eef58d21c2a
Andrew Fry <andrew.fry@neddie.ploogie.co.uk>
2023-11-20 10:11:45 +0000

    printat() now caters for multi input such as for print()/println()

--- evaluator/printat.go
+++ evaluator/printat.go
@@ -3,14 +3,25 @@ package evaluator
 import (
 	"fmt"
 	"mwnci/object"
+	"mwnci/typing"
 )
 
-// output a string to stdout
 func Printat(args ...object.Object) object.Object {
+	if err := typing.Check(
+		"printat", args,
+		typing.MinimumArgs(3),
+		typing.WithTypes(object.INTEGER_OBJ, object.INTEGER_OBJ),
+	); err != nil {
+		return newError(err.Error())
+	}
 	X := int(args[0].(*object.Integer).Value)
 	Y := int(args[1].(*object.Integer).Value)
-	Text := args[2].Inspect()
-	fmt.Print("\033[", X, ";", Y, "H", Text, "\033[0m")
+	fmt.Print("\033[", X, ";", Y, "H")
+	for _, v := range args[2:] {
+		foo := v.ToInterface()
+		fmt.Print(foo)
+	}
+	fmt.Print("\033[0m")
 
 	return NULL
 }
