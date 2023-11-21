1d26aded74cf2dae25d5946922a593c795979395
Andrew Fry <andrew.fry@neddie.ploogie.co.uk>
2023-11-21 14:54:00 +0000

    Further bloat removal

--- evaluator/println.go
+++ evaluator/println.go
@@ -7,9 +7,6 @@ import (
 
 // Println ...
 func Println(args ...object.Object) object.Object {
-	//	for _, s := range args {
-	//	fmt.Print(s)
-	//	}
 	putsFun(args...)
 	fmt.Print("\n")
 	return NULL
