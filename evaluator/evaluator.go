3dde41d8f21e290a176d79b4d053e568efb3425d
Andrew Fry <andrew.fry@neddie.ploogie.co.uk>
2023-12-06 21:34:51 +0000

    Added more for units()

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -1004,6 +1004,9 @@ func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object
 	if builtin, ok := builtins[node.Value]; ok {
 		return builtin
 	}
+	fmt.Fprintf(os.Stderr, "identifier not found: %s\n", node.Value)
+        os.Exit(1)
+
 	return newError("identifier not found: " + node.Value)
 }
 
