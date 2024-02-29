2b2fdb044ff9a2c6d1b72b1bd6e85790b4eca785
Andrew Fry <andrew.fry@neddie.ploogie.co.uk>
2024-02-29 09:12:14 +0000

    Removed function tests

--- evaluator/assert.go
+++ evaluator/assert.go
@@ -11,15 +11,18 @@ import (
 func Assert(args ...object.Object) object.Object {
 	if err := typing.Check(
 		"assert", args,
-		typing.ExactArgs(2),
+		typing.RangeOfArgs(2,3),
 		typing.WithTypes(object.BOOLEAN_OBJ, object.STRING_OBJ),
 	); err != nil {
 		return newError(err.Error())
 	}
 
 	if !args[0].(*object.Boolean).Value {
+		if len(args) == 3 {
+			return FALSE
+		}
 		fmt.Printf("Assertion Error: %s\n", args[1].(*object.String).Value)
 		os.Exit(1)
 	}
-	return nil
+	return TRUE
 }
