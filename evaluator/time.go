470a8885c1e6065ee2371cc0f862e3073abd8db9
Andrew Fry <andrew.fry@neddie.ploogie.co.uk>
2024-10-22 05:45:55 +0100

    Buitltin time() now accepts milli, micro, nano, unix for output type

--- evaluator/time.go
+++ evaluator/time.go
@@ -1,6 +1,7 @@
 package evaluator
 
 import (
+	"strings"
 	"time"
 	"mwnci/object"
 	"mwnci/typing"
@@ -10,11 +11,26 @@ import (
 func Time(args ...object.Object) object.Object {
 	if err := typing.Check(
 		"time", args,
-		typing.ExactArgs(0),
+		typing.RangeOfArgs(0,1),
+		typing.WithTypes(object.STRING_OBJ),
 	); err != nil {
 		return newError(err.Error())
 	}
 	now := time.Now()
+	if len(args) == 1  {
+		TimeType := strings.ToLower(args[0].(*object.String).Value)
+		switch TimeType {
+		case "milli" :
+			return &object.Integer{Value: now.UnixMilli()}
+		case "micro" :
+			return &object.Integer{Value: now.UnixMicro()}
+		case "nano" :
+			return &object.Integer{Value: now.UnixNano()}
+		case "unix" :
+			return &object.Integer{Value: now.Unix()}
+		default:
+			return newError("argument to time() not supported, got=%s", TimeType)
+		}
+	}
 	return &object.Integer{Value: now.Unix()}
 }
-
