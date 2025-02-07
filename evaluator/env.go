9596fd31b8609d80211a2febf6cb142febbc0a5f
Andrew Fry <andrew.fry@neddie.ploogie.co.uk>
2025-02-07 15:13:53 +0000

    getenv(), env(), netstat() no longer use system() to get the data

---
+++ evaluator/env.go
@@ -0,0 +1,27 @@
+package evaluator
+
+import (
+	"os"
+	"strings"
+	"mwnci/object"
+	"mwnci/typing"
+)
+
+func EnvFun(args ...object.Object) object.Object {
+	if err := typing.Check(
+		"env", args,
+		typing.ExactArgs(0),
+	); err != nil {
+		return newError(err.Error())
+	}
+	newHash := make(map[object.HashKey]object.HashPair)
+	for _, value := range os.Environ() {
+		SplitVar:=strings.Split(value, "=")
+		key := &object.String{Value: SplitVar[0]}
+		val := &object.String{Value: SplitVar[1]}
+		newHashPair := object.HashPair{Key: key, Value: val}
+		newHash[key.HashKey()] = newHashPair
+	}
+	return &object.Hash{Pairs: newHash}
+}
+
