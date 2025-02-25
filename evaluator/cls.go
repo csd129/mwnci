50915c3ba24497ed6951dc45f7d33b175421a67d
Andrew Fry <andrew.fry@ploogie.co.uk>
2025-02-25 14:39:07 +0000

    cls() now uses control codes rather than system()

--- evaluator/cls.go
+++ evaluator/cls.go
@@ -1,8 +1,7 @@
 package evaluator
 
 import (
-	"os"
-	"os/exec"
+	"fmt"
 	"mwnci/object"
 	"mwnci/typing"
 )
@@ -14,9 +13,12 @@ func Cls(args ...object.Object) object.Object {
 	); err != nil {
 		return newError(err.Error())
 	}
-	var cmd *exec.Cmd
-	cmd = exec.Command("clear")
-	cmd.Stdout = os.Stdout
-	cmd.Run()
+
+	//
+	// To execute "clear", or not
+	// There's really no decisive way. So will use
+	// escape codes cos this is mainly written for
+	// linux at the mo.
+	fmt.Print("\033[H\033[2J")
 	return nil
 }
