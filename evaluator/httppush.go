de53255595619a48c43dfe3860821f06ecb03cea
Andrew Fry <andrew.fry@neddie.ploogie.co.uk>
2025-03-25 21:25:20 +0000

    httppost() now uses environment variable HTTP_PROXY alongside httpget()

--- evaluator/httppush.go
+++ evaluator/httppush.go
@@ -1,11 +1,13 @@
 package evaluator
 
 import (
+	"os"
 	"bytes"
 	"io"
+	"net/http"
+	"net/url"
 	"mwnci/object"
 	"mwnci/typing"
-	"net/http"
 )
 
 func HttpPost(args ...object.Object) object.Object {
@@ -16,16 +18,20 @@ func HttpPost(args ...object.Object) object.Object {
 	); err != nil {
 		return newError(err.Error())
 	}
+	PROXY:=os.Getenv("HTTP_PROXY")
+	proxyURL, _ := url.Parse(PROXY)
 	url := args[0].(*object.String).Value
 	content := args[1].(*object.String).Value
 	data := args[2].(*object.String).Value
-
-	resp, err := http.Post(url, content, bytes.NewBuffer([]byte(data)))
-
+	proxy := http.ProxyURL(proxyURL)
+	transport := &http.Transport{Proxy: proxy}
+	client := &http.Client{Transport: transport}
+	req, _ := http.NewRequest("POST", url, bytes.NewBuffer([]byte(data)))
+	req.Header.Add("Content-Type", content)
+	resp, err := client.Do(req)
 	if err != nil {
 		return newError(err.Error())
 	}
-	defer resp.Body.Close()
 	b, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return newError(err.Error())
