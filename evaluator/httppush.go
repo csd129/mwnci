0fa61add93c82b69c82abdcc0190f3a5db42000c
Andrew Fry <andrew.fry@neddie.ploogie.co.uk>
2025-03-26 10:43:40 +0000

    Fixed httppost() when HTTP_PROXY is not set

--- evaluator/httppush.go
+++ evaluator/httppush.go
@@ -10,6 +10,22 @@ import (
 	"mwnci/typing"
 )
 
+func HttpPostNoProxy(args ...object.Object) object.Object {
+	url := args[0].(*object.String).Value
+	content := args[1].(*object.String).Value
+	data := args[2].(*object.String).Value
+	resp, err := http.Post(url, content, bytes.NewBuffer([]byte(data)))
+	if err != nil {
+		return newError(err.Error())
+	}
+	defer resp.Body.Close()
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return newError(err.Error())
+	}
+	return &object.String{Value: string(b)}
+}
+
 func HttpPost(args ...object.Object) object.Object {
 	if err := typing.Check(
 		"httppost", args,
@@ -19,6 +35,9 @@ func HttpPost(args ...object.Object) object.Object {
 		return newError(err.Error())
 	}
 	PROXY:=os.Getenv("HTTP_PROXY")
+	if len(PROXY) == 0 {
+		return HttpPostNoProxy(args[0], args[1], args[2])
+	}
 	proxyURL, _ := url.Parse(PROXY)
 	url := args[0].(*object.String).Value
 	content := args[1].(*object.String).Value
