5bfd8239d2e7f3be61b2ec12461c7148ae6423b5
Andrew Fry <andrew.fry@ploogie.co.uk>
2025-07-17 10:26:06 +0100

    Added error checking to unzip()

--- evaluator/unzip.go
+++ evaluator/unzip.go
@@ -15,6 +15,9 @@ func Unzip(args ...object.Object) object.Object {
 	}
 
 	Array := args[0].(*object.Array).Elements
+	if Array[0].Type() != object.ARRAY_OBJ {
+		return newError("argument to unzip() not supported, expected ARRAY[ARRAY], got=ARRAY[%s]", Array[0].Type())
+	}
 	ABlock := Array[0].(*object.Array).Elements
 	DataLen := len(ABlock)
 	ZipArray := make([]object.Object, DataLen)
@@ -23,10 +26,14 @@ func Unzip(args ...object.Object) object.Object {
 
 	for Index := 0; Index < DataLen; Index++ {
 		for In, Block := range Array  {
-		    NBlock := Block.(*object.Array).Elements
-		    TempArray[In] = NBlock[Index]
-		    UnMute:=&object.Array{Elements: TempArray}
-		    ZipArray[Index]=UnMute.Copy()
+			if Block.Type() == object.ARRAY_OBJ {
+				NBlock := Block.(*object.Array).Elements
+				TempArray[In] = NBlock[Index]
+				UnMute:=&object.Array{Elements: TempArray}
+				ZipArray[Index]=UnMute.Copy()
+			} else {
+				return newError("argument to unzip() not supported, expected ARRAY[ARRAY], got=ARRAY[%s]", Block.Type())
+			}
 		}
 	}
 	return &object.Array{Elements: ZipArray}
