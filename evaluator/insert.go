dd20ec2c797b54f8249212c9b6aff06980afc93e
Andrew Fry <andrew.fry@neddie.ploogie.co.uk>
2025-08-05 14:37:56 +0100

    insert() now allows insertions into a string.

--- evaluator/insert.go
+++ evaluator/insert.go
@@ -1,6 +1,7 @@
 package evaluator
 
 import (
+	//	"fmt"
 	"mwnci/object"
 	"mwnci/typing"
 )
@@ -9,21 +10,35 @@ func ArrayInsert(args ...object.Object) object.Object {
 	if err := typing.Check(
 		"insert", args,
 		typing.ExactArgs(3),
-		typing.WithTypes(object.ARRAY_OBJ, object.INTEGER_OBJ),
+		//typing.WithTypes(object.ARRAY_OBJ, object.INTEGER_OBJ),
 	); err != nil {
 		return newError(err.Error())
 	}
-	arr := args[0].(*object.Array)
-	newArray := arr.Copy()
-	elem := int(args[1].(*object.Integer).Value)
 
-	if (elem > len(newArray.Elements)-1) || (elem < 0) {
-		return newError("IndexError: array index [%d] out of range ", elem)
-	} else {
-		val := args[2]
-		newArray.Insert(elem, val)
+	if args[0].Type() == object.ARRAY_OBJ {
+		arr := args[0].(*object.Array)
+		newArray := arr.Copy()
+		elem := int(args[1].(*object.Integer).Value)
+
+		if (elem > len(newArray.Elements)-1) || (elem < 0) {
+			return newError("IndexError: array index [%d] out of range ", elem)
+		} else {
+			val := args[2]
+			newArray.Insert(elem, val)
+		}
+		return newArray
 	}
-	return newArray
+	if args[0].Type() == object.STRING_OBJ {
+		text := args[0].(*object.String).Value
+		elem := int(args[1].(*object.Integer).Value)
+		val:=args[2].(*object.String).Value
+		if (elem > len(text)-1) || (elem < 0) {
+			return newError("IndexError: string index [%d] out of range ", elem)
+		}
+		Newtext := text[:elem] + val  + text[elem:]
+		return &object.String{Value: Newtext} 
+	}
+	return newError("argument to insert() not supported, expected HASH, ARRAY or STRING, got=%s", args[0].Type())
 
 }
 
