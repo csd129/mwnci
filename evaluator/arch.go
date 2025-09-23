a276096c85dc68e231eda174b165eaa9bb001a78
Andrew Fry <andrew.fry@neddie.ploogie.co.uk>
2025-09-23 04:12:42 +0100

    Added arch() builltin and updated getcpu() for bsd to show results after 2 cycles for a batter result

---
+++ evaluator/arch.go
@@ -0,0 +1,18 @@
+package evaluator
+
+import (
+	"mwnci/object"
+	"mwnci/typing"
+	"runtime"
+)
+
+func Arch(args ...object.Object) object.Object {
+	if err := typing.Check(
+		"arch", args,
+		typing.ExactArgs(0),
+	); err != nil {
+		return newError(err.Error())
+	}
+	return &object.String{Value: runtime.GOARCH}
+}
+
