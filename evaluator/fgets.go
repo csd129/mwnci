8befdf30813e4bb499af2561e87620c773277aad
Andrew Fry <andrew.fry@ploogie.co.uk>
2026-08-11 05:12:54 +0100

    Re-write of fgets() so that it does things properly

--- evaluator/fgets.go
+++ evaluator/fgets.go
@@ -3,38 +3,50 @@ package evaluator
 import ( 
 	"mwnci/object"
 	"mwnci/typing"
+	"strings"
 	"syscall"
 )
 
 func Fgets(args ...object.Object) object.Object {
 	if err := typing.Check(
 		"fgets", args,
-		typing.ExactArgs(1),
-		typing.WithTypes(object.INTEGER_OBJ),
+		typing.RangeOfArgs(1,2),
+		typing.WithTypes(object.INTEGER_OBJ, object.INTEGER_OBJ),
 	); err != nil {
 		return newError("%s", err.Error())
 	}
 
 	var (
 		fd int
-		n  = 1
+		n  = DefaultBufferSize
 	)
 
 	fd = int(args[0].(*object.Integer).Value)
-
 	if len(args) == 2 {
 		n = int(args[1].(*object.Integer).Value)
 	}
+
 	fullstring := ""
 	buf := make([]byte, n)
-        for string(buf[:n]) != "\n" {
-		n, err := syscall.Read(fd, buf)
-                if len(buf[:n]) == 0 {return FALSE}
-		if err != nil {
-			return newError("IOError: %s", err)
-		}
-		charbyte := string(buf[:n])
-		fullstring = fullstring + charbyte
-        }
+        offset, err := syscall.Seek(fd, 0, 1)
+	if err != nil {
+		return newError("IOError: %s", err)
+	}
+	n, err = syscall.Read(fd, buf)
+	if len(buf[:n]) == 0 {return FALSE}
+	if err != nil {
+		return newError("IOError: %s", err)
+	}
+	index := strings.Index(string(buf), "\n")
+	if index == -1 {
+		fullstring=string(buf)
+	} else {
+		fullstring=string(buf[:index])
+	}
+	offset=offset + int64(len(fullstring)) + 1
+	_, err = syscall.Seek(fd, offset, 0)
+	if err != nil {
+		return newError("IOError: %s", err)
+	}
 	return &object.String{Value: fullstring}
 }
